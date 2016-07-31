00a8491a4b26d3011234f6efe0ee3c01a4a0a486
yisa <523802938@qq.com>
2016-07-31 21:11:09 +0800

    练习题10:优化宠物狗这个类，新建一个动物的类，使宠物狗继这个类。并且实现宠物狗的特有的行为。

--- test10.py
+++ test10.py
@@ -1,13 +1,13 @@
-class animal:
+class Animal:
     def __init__(self, name, sex, age):
        self.name = name
        self.sex = sex
        self.age = age
-class dog(animal):
+class Dog(Animal):
     def __init__(self, name, sex, age, number):
-       animal.__init__(self, name, sex, age)
+       Animal.__init__(self, name, sex, age)
        self.number = number
     def eat(self):
        return s.name + 'is eating'
-s = dog('wangcai', 'male', 8, 110)
+s = Dog('wangcai', 'male', 8, 110)
 print s.eat() 
